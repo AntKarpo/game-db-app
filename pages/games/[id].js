ecbb3c804f5f4cfac64be132865e96603cae2d63
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-10-19 16:54:32 +0200

    created screenshots for the details page

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -1,19 +1,32 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "./id.module.css"
 
 const GameDetails = () => {
   const router = useRouter();
-  const { id, name, image, metascore } = router.query;
+  const { id, name, image, metascore, screenshots } = router.query;
+  
+  console.log(screenshots, "screenshots");
+  console.log(name,"name");
+const screenshotss = JSON.parse(screenshots);
+
   return (
-    <div>
-      <h1> {name}</h1>
-      <img
-        src={image}
-        alt={`Image for ${name}`}
-        width={300}
-        height={300}
-      />
-      <p>Metascore rating: {metascore}</p>
+    <div className={styles.detailsCard}>
+      <h1>{name}</h1>
+      <img src={image} alt={`Image for ${name}`} width={300} height={300} />
+      <p>Metascore rating: {metascore}</p><br/>
+      <div className={styles.screenshotsContainer}>
+        {console.log(screenshots, "inline screenshot")}
+        {screenshotss?.map((screenshot, index) => (
+            <img
+              key={index}
+              src={screenshot}
+              alt={`Screenshot for ${name}`}
+              width={150}
+              height={120}
+              />
+              ))}
+      </div>
     </div>
   );
 };
--- pages/games/index.js
+++ pages/games/index.js
@@ -1,35 +1,42 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "./Games.module.css";
 
+const fetchGames = async (page) => {
+  try {
+    const response = await fetch(
+      `https://api.rawg.io/api/games?key=27257cb0d3da4a78b072bd0bc225adb6&page=${page}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    const data = await response.json();
+  console.log("data",data);
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
 const GamePage = () => {
   const [gameList, setGameList] = useState([]);
-  const [page, setPage] = useState(0); 
+  const [page, setPage] = useState(1); 
 
 
-  const fetchGames = useCallback(async () => {
-    try {
-      const response = await fetch(
-        `https://api.rawg.io/api/games?key=27257cb0d3da4a78b072bd0bc225adb6&page=${page}`
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
-      }
-      const data = await response.json();
-      setGameList((prevGameList) => [...prevGameList, ...data.results]);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }, [page]);
 
   useEffect(() => {
-    fetchGames();
-  }, [fetchGames]);
+    async function fetchData(){
+   const newGames = await fetchGames(page);
+  console.log("newGames",newGames);
+          setGameList([...gameList, ...newGames])}
+          fetchData()
+}, [page]);
 
   const loadMoreGames = () => {
     setPage(page + 1);
   };
-console.log(gameList);
+
+if(!gameList) return
   return (
     <main>
       <h2>All the Games in One Place</h2>
@@ -37,9 +44,13 @@ console.log(gameList);
         {gameList.map((game) => (
           <div key={game.id} className={styles.gameCard}>
             <h3>{game.name}</h3>
-            <Link href={`/games/${game.id}?name=${game.name}&image=${game.background_image}&metascore=${game.metacritic
-}`} key={game.id}>
-    <img
+            <Link
+            href={`/games/${game.id}?name=${game.name}&image=${game.background_image}&metascore=${game.metacritic}&screenshots=${JSON.stringify(
+              game.short_screenshots.map((screenshot) => screenshot.image))}`}
+              key={game.id}
+              >
+      
+            <img
       src={game.background_image}
       width={200}
       height={200}
@@ -49,7 +60,9 @@ console.log(gameList);
           </div>
         ))}
       </div>
+<div className={styles.centeredButton}>
       <button onClick={loadMoreGames}>Load More Games</button>
+      </div>
     </main>
   );
 };
